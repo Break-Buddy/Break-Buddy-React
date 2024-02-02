bd6993dfbfab48e7f13c4265f91b693e067d4cc4
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-02 04:01:29 -0600

    Set up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <h1 className="text-blue-500">BREAK BUDDY</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
