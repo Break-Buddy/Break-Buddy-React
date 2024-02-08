9f1a55198730bcb1dc32d6473517755dd07ed719
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-08 12:05:04 -0600

    merge FAQ

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,3 +1,16 @@
+import FAQ from "../components/FAQ";
+import LandingDisplay from "../components/LandingDisplay";
+
+function Home() {
+  return (
+    <div>
+      <LandingDisplay />
+      <FAQ />
+    </div>
+  );
+}
+
+export default Home;
 import HowItWorks from "../components/HowItWorks";
 import LandingDisplay from "../components/LandingDisplay";
 
