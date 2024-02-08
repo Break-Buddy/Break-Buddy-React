5b83415d0710066328af6fe3acb3ddce1aeedb48
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-08 12:29:06 -0600

    merged again

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
-=======
 import HowItWorks from "../components/HowItWorks";
 import FAQ from "../components/FAQ";
->>>>>>> c2fe1852d36c0cfc3f89dd3e08d9c912f4777200
 import LandingDisplay from "../components/LandingDisplay";
 
 function Home() {
   return (
     <div>
       <LandingDisplay />
-<<<<<<< HEAD
-=======
       <HowItWorks />
       <FAQ />
->>>>>>> c2fe1852d36c0cfc3f89dd3e08d9c912f4777200
     </div>
   );
 }
