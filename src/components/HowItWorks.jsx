bfea92d7594c81783cd568d3f026669700ab27ab
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-08 03:05:43 -0600

    created how it works section

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,38 +1,45 @@
+import create from "../assets/iconCreate.png";
+import join from "../assets/iconJoin.png";
+import meet from "../assets/iconMeet.png";
+
 function HowItWorks() {
   const instructions = [
     {
       number: 1,
       title: "Create a break",
       text: "Get creative and post a break as an invitation",
-      icon: "icon",
+      icon: create,
     },
     {
       number: 2,
       title: "Join a break",
       text: "Be inspired and find likeminded remote workers",
-      icon: "icon",
+      icon: join,
     },
     {
       number: 3,
       title: "Meet for your break",
       text: "Be punctual and meet your break buddy",
-      icon: "icon",
+      icon: meet,
     },
   ];
 
   return (
-    <div className="">
-      <div>
+    <div className="pt-12 pb-28 flex justify-center bg-[#EBF6FF]">
+      <div className="flex flex-col justify-center items-center w-fit px-12 gap-12">
         <h2>HOW IT WORKS</h2>
-        <div>
+        <div className="flex flex-col gap-5">
           {instructions.map((instruction, i) => (
-            <div key={i}>
-              <div>{instruction.number}</div>
-              <div>
-                <h2>{instruction.title}</h2>
-                <h3>{instruction.text}</h3>
+            <div key={i} className="flex items-center gap-11">
+              <div className="relative flex justify-center items-center">
+                <span className="absolute rounded-full border-[#007DE2] border-2 w-12 h-12"></span>
+                <h3>{instruction.number}</h3>
               </div>
-              <div>{instruction.icon}</div>
+              <div className="flex flex-col gap-2">
+                <h2>{instruction.title}</h2>
+                <h3 className="w-96">{instruction.text}</h3>
+              </div>
+              <img src={instruction.icon}></img>
             </div>
           ))}
           <div>
