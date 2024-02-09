2557f5286044743c979bbed6a9761ef9749e5c31
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-09 01:18:12 -0600

    changed landing photo

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -1,12 +1,14 @@
 import landingImage1 from "../assets/landingImage1.png";
 import landingImage2 from "../assets/landingImage2.png";
+import headerphotoo from "../assets/headerphotoo.svg";
 
 function LandingDisplay() {
   return (
     <div className="px-16 pt-20 flex flex-col justify-center">
-      <div className="flex justify-center px-64 py-36 items-center gap-20">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-col gap-2">
+      <div className="flex justify-center px-80 py-6 items-center gap-2">
+        {/* LEFT SIDE */}
+        <div className="flex flex-col gap-4 w-1/2">
+          <div className="flex flex-col justify-start gap-2">
             <h1 className="text- font-bold">Reward yourself.</h1>
             <h1 className="font-bold">Take a social break</h1>
           </div>
@@ -15,25 +17,12 @@ function LandingDisplay() {
           </h2>
           <button className="button-1 px-[18px] py-[12px]">Start</button>
         </div>
-        <div className="w-1/2 h-full relative -top-56">
-          <img
-            className="w-[214px] h-[281px] absolute left-12 z-20"
-            src={landingImage1}
-            alt="Guy Cooking 1"
-          />
-          <img
-            className="w-[253px] h-[256px] absolute left-40 top-36 z-10"
-            src={landingImage2}
-            alt="Guy Cooking 2"
-          />
-          <span className="landingCaption top-6 left-6">COOK</span>
-          <span className="landingCaption top-14 left-52">TAKE A WALK</span>
-          <span className="landingCaption top-52 left-1">EAT LUNCH</span>
-          <span className="landingCaption top-72 left-[370px]">MAKE ART</span>
-          <span className="py-3 px-7 text-base rounded-full absolute z-30 bg-[#FFF] top-[375px] left-56">
-            TOGETHER
-          </span>
+        {/* END LEFT SIDE */}
+        {/* RIGHT SIDE */}
+        <div className="w-1/2 h-full flex">
+          <img src={headerphotoo} alt="" />
         </div>
+        {/* END RIGHT SIDE */}
       </div>
     </div>
   );
