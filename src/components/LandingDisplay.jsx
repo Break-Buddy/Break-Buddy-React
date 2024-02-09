5025d3080cbd9d1206b6a1038b064b6fa6c5c4ec
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-09 02:29:50 -0600

    adjusted padding for mobile

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -4,24 +4,24 @@ import headerphotoo from "../assets/headerphotoo.svg";
 
 function LandingDisplay() {
   return (
-    <div className="px-0 md:px-16 pt-20 flex flex-col justify-center">
-      <div className="flex flex-col-reverse md:flex-row justify-center md:px-80 py-6 items-center gap-2">
+    <div className="px-5 md:px-16 pt-20 flex flex-col justify-center items-center">
+      <div className="flex flex-col-reverse md:flex-row justify-center md:px-80 py-6 md:items-center gap-2">
         {/* LEFT SIDE */}
-        <div className="flex flex-col gap-4 px-20 md:px-0 lg:w-1/2">
+        <div className="flex flex-col gap-4 px-14 md:px-0 lg:w-1/2">
           <div className="flex flex-col justify-start gap-2">
             <h1 className="text-3xl lg:text-5xl font-bold">Reward yourself.</h1>
             <h1 className="text-3xl lg:text-5xl font-bold">
               Take a social break
             </h1>
           </div>
-          <h2 className="w-[420px]">
+          <h2 className=" md:w-[420px] text-base md:text-2xl">
             Find an accountability buddy for taking that much needed break
           </h2>
-          <button className="button-1 px-[18px] py-[12px]">Start</button>
+          <button className="button-1 text-base px-[18px] py-[12px]">Start</button>
         </div>
         {/* END LEFT SIDE */}
         {/* RIGHT SIDE */}
-        <div className="px-8 w-full md:px-0 lg:w-1/2 h-full flex">
+        <div className="px-8 w-full md:px-0 lg:w-1/2 h-full flex justify-center">
           <img src={headerphotoo} alt="" />
         </div>
         {/* END RIGHT SIDE */}
