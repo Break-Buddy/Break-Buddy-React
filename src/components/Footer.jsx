e0462a3d6b165f600e233c040f7cf594d4e209ed
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-09 03:10:52 -0600

    changed footer logo, adjusted mobile view

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
   ];
   return (
     <div className="bg-[#003F71] px-9 py-8">
-      <div className="flex items-center justify-between">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12 md:gap-0">
         <img src={BBLogo2} alt="" />
-        <div className="flex flex-col gap-6 text-white">
+        <div className="flex flex-col gap-6 text-white items-center md:items-start">
           {footerLinks.map((link, i) => (
             <div key={i}>
               <a href="#">{link.title}</a>
