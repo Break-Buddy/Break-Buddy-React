49110133d81995d35333b8b2c3814f822ab446ad
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-09 01:31:19 -0600

    mobile view padding and changed hover color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import BBLogo from "../assets/BBLogo1.png";
 
 function Navbar() {
   return (
-    <div className="absolute flex justify-between items-center w-full py-3 px-40">
+    <div className="absolute flex justify-between items-center w-full py-3 px-12 xl:px-40">
       <div>
         <img src={BBLogo} className="w-28" alt="Break Buddy Logo" />
       </div>
       <div className="flex gap-6 items-center">
         <h3 className="text-[#007DE2]">Sign In</h3>
-        <h3 className="border text-white px-4 py-2 rounded-[4px] bg-[#007DE2] hover:bg-gray-400 cursor-pointer  transition-all duration-200">
+        <h3 className="border text-white px-4 py-2 rounded-[4px] bg-[#007DE2] hover:bg-[#00589E] cursor-pointer  transition-all duration-200">
           Join for Free
         </h3>
       </div>
