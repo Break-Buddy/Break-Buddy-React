dda38105d9bcee5417ccdc4e6be906582f2beaf2
Constance Dubose <cdubose22@gmail.com>
2024-02-11 03:03:28 -0500

    removed the signup and signin paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Home from "./views/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Dashboard from "./views/Dashboard";
-import SignInPage from "./components/SignInPage";
-import SignUpPage from "./components/SignUpPage"; // Corrected import name
 
 function App() {
   return (
@@ -14,9 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Dashboard />} /> {/* Route for Dashboard */}
-        <Route path="/signin" element={<SignInPage />} /> {/* Route for SignInPage */}
-        <Route path="/signup" element={<SignUpPage />} /> {/* Route for SignUpPage */}
-      </Routes>
+       </Routes>
       <Footer />
     </div>
   );
