4329179bf51e9eca1171b9661f10aacb66ea1c77
Jose Laurito <jigolaurito93@yahoo.com>
2024-02-26 00:29:15 -0600

    adjusted image width for hero

--- src/components/LandingDisplay.jsx
+++ src/components/LandingDisplay.jsx
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 
 function LandingDisplay() {
   return (
-    <div className="px-5 md:px-16 pt-20 flex flex-col justify-center items-center">
-      <div className="flex flex-col-reverse md:flex-row justify-center md:px-80 py-6 md:items-center gap-40">
+    <div className="px-2 pt-20 pb-5 flex justify-center items-center lg:px-20">
+      <div className="flex flex-col lg:flex-row justify-center py-6 md:items-center gap-10 lg:gap-1">
         {/* LEFT SIDE */}
-        <div className="flex flex-col gap-4 px-14 md:px-0 lg:w-1/2">
+        <div className="flex flex-col gap-4 w-full px-14 md:px-5 order-2 lg:order-1 lg:w-1/2">
           <div className="flex flex-col justify-start gap-2">
-            <h1 className="text-[26px] lg:text-5xl font-bold">
+            <h1 className="text-[29px] md:text-[43px] font-bold">
               Reward yourself.
             </h1>
-            <h1 className="text-[26px] lg:text-5xl font-bold">
+            <h1 className="text-[29px] md:text-[43px] font-bold">
               Take a social break
             </h1>
           </div>
-          <h2 className=" md:w-[420px] text-base md:text-2xl">
+          <h2 className=" md:w-[420px] text-base md:text-xl">
             Find an accountability buddy for taking that much needed break
           </h2>
           <Link to="/signup">
@@ -26,7 +26,7 @@ function LandingDisplay() {
         </div>
         {/* END LEFT SIDE */}
         {/* RIGHT SIDE */}
-        <div className="px-8 w-full md:px-0 lg:w-1/2 h-full flex justify-center px-">
+        <div className="px-8 h-full flex justify-center lg:order-2 lg:w-1/2">
           <img src={headerphotoo} alt="" />
         </div>
         {/* END RIGHT SIDE */}
